68299bbd6ff5289cce3ab6a0f4f6cf8d0ce68c8a
kuldeepvyass <vyask519@gmail.com>
2022-06-15 20:24:41 +0530

    best

--- app.js
+++ app.js
@@ -127,7 +127,7 @@ app.post("/delete",function(req,res){
     });
  }
 });
-const port = process.env.PORT || 3000
+const port = process.env.PORT ||3000
 app.listen(port,function(){
     console.log("connected");
 });
